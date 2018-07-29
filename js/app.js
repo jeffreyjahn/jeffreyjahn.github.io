a0a23b9669322534f27341ed095b821a1486f309
Jeffrey Ahn <jeffreja@gmail.com>
2018-07-28 17:24:17 -0700

    changing some colors, added a portfolio item

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
+$(document).ready(() => {
     var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
+    window.onscroll = () => {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("navbar").style.top = "0";
@@ -11,11 +11,58 @@ $(document).ready(function () {
     }
     new WOW().init();
 
-    $('#picsell-card').hover(function () {
-            // over
-            
-        }, function () {
+    $('#picsell-img').hover(() => {
+            // over"
+            $('#picsell-icon').addClass("show");
+            $('#picsell-img').addClass('portfolio-opacity');
+        },  () => {
+            // out
+            $('#picsell-icon').removeClass("show");
+            $('#picsell-img').removeClass('portfolio-opacity');
+        }
+    );
+    $('#pawrental-img').hover(() => {
+            // over"
+            $('#pawrental-icon').addClass("show");
+            $('#pawrental-img').addClass('portfolio-opacity');
+        }, () => {
+            // out
+            $('#pawrental-icon').removeClass("show");
+            $('#pawrental-img').removeClass('portfolio-opacity');
+        }
+    );
+    $('#portfolio-img').hover(() => {
+            // over"
+            $('#portfolio-icon').addClass("show");
+            $('#portfolio-img').addClass('portfolio-opacity');
+        }, () => {
+            // out
+            $('#portfolio-icon').removeClass("show");
+            $('#portfolio-img').removeClass('portfolio-opacity');
+        }
+    );$('#picsell-icon').hover(() => {
+            // over"
+            $('#picsell-icon').addClass("show");
+            $('#picsell-img').addClass('portfolio-opacity');
+        }, () => {
             // out
         }
     );
+    $('#pawrental-icon').hover(() => {
+            // over"
+            $('#pawrental-icon').addClass("show");
+            $('#pawrental-img').addClass('portfolio-opacity');
+        }, () => {
+            // out
+        }
+    );
+    $('#portfolio-icon').hover(() => {
+            // over"
+            $('#portfolio-icon').addClass("show");
+            $('#portfolio-img').addClass('portfolio-opacity');
+        }, () => {
+            // out
+        }
+    );
+    
 });
